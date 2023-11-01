42b3a24e6ca652745788b8fe53f92af82389bccb
BillyJoelBallola <billyjoelballola27@gmail.com>
2023-11-01 18:29:06 +0800

    minor revision

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const book = document.querySelector(".book");
 const cover = document.querySelector(".cover");
 const content = document.querySelector(".content");
 
-let isBookOpen = false 
+let isBookOpen = false;
+const flipAngle = -110;
+const rotateAngle = 5;
 
 book.addEventListener("click", () => {
   if(!isBookOpen){
-    cover.style.transform = "rotateY(-110deg)";
+    cover.style.transform = `rotateY(${flipAngle}deg)`;
     cover.style.transformOrigin = "left";
-    book.style.transform = "rotateZ(5deg)";
+    book.style.transform = `rotateZ(${rotateAngle}deg)`;
     isBookOpen = true;
   }else{
     cover.style.transform = "rotateY(0)";
