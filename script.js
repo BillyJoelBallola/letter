c529d6dd0e5de4c99c57aed43e61962661fae52e
BillyJoelBallola <billyjoelballola27@gmail.com>
2023-11-02 21:29:27 +0800

    type writing feature

--- script.js
+++ script.js
@@ -4,16 +4,45 @@ const content = document.querySelector(".content");
 const images = document.querySelectorAll(".image");
 const previewContainer = document.querySelector(".image-preview-contianer");
 const previewImage = document.querySelector(".image-preview");
+const letter = document.querySelector(".letter");
 
 let isBookOpen = false;
 const flipAngle = -110;
 const rotateAngle = 5;
+
+let i = 0;
+let txt = "Hi baby, <br/><br/>I just want you to know that my love for you is genuine and will never change. Whatever happens, you'll always have my heart. <br/><br/>Love,<br/>Bjay";
+const speed = 20;
+
+function typeWriter() {
+  if (i < txt.length) {
+    if (txt.charAt(i) === '<') {
+      const closingIndex = txt.indexOf('>', i);
+      if (closingIndex !== -1) {
+        const tag = txt.slice(i, closingIndex + 1);
+        letter.innerHTML += tag;
+        i = closingIndex + 1;
+      }
+    } else {
+      letter.innerHTML += txt.charAt(i);
+      i++;
+    }
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function resetTypeWriter() {
+  i = 0;
+  letter.innerHTML = "";
+}
 
 book.addEventListener("click", () => {
   if(!isBookOpen){
     cover.style.transform = `rotateY(${flipAngle}deg)`;
     cover.style.transformOrigin = "left";
     book.style.transform = `rotateZ(${rotateAngle}deg)`;
+    resetTypeWriter();
+    typeWriter();
     isBookOpen = true;
   }else{
     cover.style.transform = "rotateY(0)";
